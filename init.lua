6271ee104256e296e64de957a2fbb620dde93a61
Sampo Savolainen <sampo.savolainen@proton.me>
2024-11-20 18:54:28 -0800

    Enable Python formatters

--- init.lua
+++ init.lua
@@ -595,7 +595,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- python = { 'isort', 'black' },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
