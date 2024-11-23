672b77fe78f0bd93b375476df346fae3e8ff1cdd
Sampo Savolainen <sampo.savolainen@proton.me>
2024-11-23 00:41:45 -0800

    Configure Oil

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Open Oil in current working directory
+vim.keymap.set('n', '<leader>co', '<CMD>Oil --float<CR>', { desc = 'Open Oil in current working directory' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
