d431833a63a9dd8b14493bfc85486e821878b04e
Sampo Savolainen <sampo.savolainen@proton.me>
2025-01-29 16:22:29 -0800

    Only use colorizer plugin for html, css, and js files

--- lua/custom/plugins/nvim-colorizer.lua
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -2,5 +2,13 @@ return {
   'catgoose/nvim-colorizer.lua',
   event = 'BufReadPre',
   opts = { -- set to setup table
+    filetypes = {
+      'html',
+      'css',
+      'javascript',
+    },
+    user_default_option = {
+      names = false,
+    },
   },
 }
