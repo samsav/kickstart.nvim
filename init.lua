b167644b3d9c1cb6a491a0258aed414bf9e7d72e
Sampo Savolainen <sampo.savolainen@proton.me>
2025-01-29 16:21:41 -0800

    Change to tokyonight color scheme

--- init.lua
+++ init.lua
@@ -764,11 +764,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
