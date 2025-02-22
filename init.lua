498b71d63191102bf5382f56a8701dc5eb962038
Sampo Savolainen <sampo.savolainen@proton.me>
2025-02-22 12:58:19 -0800

    Add more completions

--- init.lua
+++ init.lua
@@ -681,6 +681,10 @@ require('lazy').setup({
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-emoji',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'PhilippFeO/cmp-help-tags',
     },
     config = function()
       -- See `:help cmp`
