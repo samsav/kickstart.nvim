111322a079cfb7888232e3edd5b29c739493d33d
Sampo Savolainen <sampo.savolainen@proton.me>
2025-02-24 23:37:03 -0800

    Use blink instead of nvim-cmp for completions

--- init.lua
+++ init.lua
@@ -412,7 +412,8 @@ require('lazy').setup({
       { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
+      -- 'hrsh7th/cmp-nvim-lsp',
+      { 'saghen/blink.cmp' },
     },
     config = function()
       --  This function gets run when an LSP attaches to a particular buffer.
@@ -524,8 +525,9 @@ require('lazy').setup({
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
       -- Enable the following language servers
       --
       --  Add any additional override configuration in the following tables. Available keys are:
