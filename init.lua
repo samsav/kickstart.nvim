8b06d240c229ef4001a84a7550bebafcec487b55
Omri Sarig <omri.sarig13@gmail.com>
2025-05-11 02:29:04 +0200

    fix: rename vim.highlight.on_yank to vim.hl.on_yank (#1482)
    The functions of vim.highlight were renamed to vim.hl on commit
    18b43c331d8a0ed87d7cbefe2a18543b8e4ad360 of neovim, which was applied
    with the release of nvim version 0.11.
    
    Now, the use of vim.highlight is deprecated, and instead, one should
    use vim.hl functions.
    In practice, vim.highlight is still working, however, asking for help
    for vim.highlight.on_yank fails (E149), while asking for help for
    vim.hl.on_yank works as expected. So, by updating the used function, a
    new user will have easier time looking getting the relevant help.
    
    Co-authored-by: Omri Sarig <omri.sarig@prevas.dk>

--- init.lua
+++ init.lua
@@ -162,12 +162,12 @@ vim.keymap.set('n', '¸', '<cmd>AerialNavToggle<CR>', { desc = 'Toggle aerial na
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
