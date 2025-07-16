ba3932f6c98adfff9581764f0f2ece1a4a2709c6
Sampo Savolainen <sampo.savolainen@proton.me>
2025-07-15 22:07:26 -0700

    Add treesitter-textobjects

---
+++ lua/custom/plugins/treesitter-textobjects.lua
@@ -0,0 +1,31 @@
+return {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+        lsp_interop = {
+          enable = true,
+          border = 'none',
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ['<leader>df'] = '@function.outer',
+            ['<leader>dF'] = '@class.outer',
+          },
+        },
+      },
+    }
+  end,
+}
