aa2439c44783380811d23892888007205bd3327a
Sampo Savolainen <sampo.savolainen@proton.me>
2026-03-23 20:03:12 -0700

    Rename conform to autoformat

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -12,6 +12,8 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
   },
+  ---@module 'conform'
+  ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
@@ -19,16 +21,14 @@ return { -- Autoformat
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
       if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
+        return nil
       else
-        lsp_format_opt = 'fallback'
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
       end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
